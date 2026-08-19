471fce10b32c82d6e79d8b224b938d2913a35d6b
antoniaspitzer <s2410238084@fhooe.at>
2026-08-19 11:49:36 +0200

    finished first version of the Timer Class

---
+++ src/main/java/com/eggtimer/model/Timer.java
@@ -0,0 +1,24 @@
+package java.com.eggtimer.model;
+
+public class Timer {
+
+    private int remainingSeconds;
+
+    public Timer(int minutes) {
+        this.remainingSeconds = minutes * 60;
+    }
+
+    public int getRemainingSeconds() {
+        return remainingSeconds;
+    }
+
+    public void tick() {
+        if (remainingSeconds > 0) {
+            remainingSeconds--;
+        }
+    }
+
+    public boolean isFinished() {
+        return remainingSeconds == 0;
+    }
+}
