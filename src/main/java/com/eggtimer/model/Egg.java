dbbe5cb5697cda2af58aca1d39dea23bca6c9c17
Antonia Spitzer <s2410238084@fhooe.at>
2026-08-28 22:33:37 +0200

    Egg.java erstellen

---
+++ src/main/java/com/eggtimer/model/Egg.java
@@ -0,0 +1,11 @@
+// this will be the file for the egg breeding animation
+
+public class Egg {
+}
+
+// there will be an animated part
+// which is abhängig from the time
+
+
+// here will be an method for randomized
+// pets which will schlüpfen 
