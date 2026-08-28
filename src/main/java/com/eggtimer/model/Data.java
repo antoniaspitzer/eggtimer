75582971f75d81d0272afdf3af8edfec9e3ae387
Antonia Spitzer <s2410238084@fhooe.at>
2026-08-28 22:35:56 +0200

    Data.java erstellen

---
+++ src/main/java/com/eggtimer/model/Data.java
@@ -0,0 +1,9 @@
+public class Data {
+
+}
+
+// here will be a methpd which determines 
+// how many pets you already have schlüpft
+
+// here will be a methpd whivh shows how 
+// much time you've spent on breeding eggies
