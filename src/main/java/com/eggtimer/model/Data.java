b332e9bff9474aea6efb7137bb382b482ec78570
Antonia Spitzer <s2410238084@fhooe.at>
2026-08-29 18:51:10 +0200

    Data.java aktualisieren

--- src/main/java/com/eggtimer/model/Data.java
+++ src/main/java/com/eggtimer/model/Data.java
@@ -7,3 +7,8 @@ public class Data {
 
 // here will be a methpd whivh shows how 
 // much time you've spent on breeding eggies
+
+// maybe you can even have different 
+// tags for example "studying hyp" 
+// and you can see the hours you spent on 
+// the different tags
