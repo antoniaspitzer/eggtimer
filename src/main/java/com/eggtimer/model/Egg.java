07f443dd7ccdf26fa09a8a2e3f7c5e0c4328293d
Antonia Spitzer <s2410238084@fhooe.at>
2026-08-29 18:48:42 +0200

    Egg.java aktualisieren

--- src/main/java/com/eggtimer/model/Egg.java
+++ src/main/java/com/eggtimer/model/Egg.java
@@ -6,6 +6,17 @@ public class Egg {
 // there will be an animated part
 // which is abhängig from the time
 
+// For that I'll need to get the percentage
+// of current time in relation to the 
+// whole timer time. 
+// I'll need ~5 different pictures from the
+// cracking egg to show the process 
+// maybe i'll animate it that all 3 seconds 
+// it shakes a little bit to signal 
+// that the pet is working his way out
+
 
 // here will be an method for randomized
 // pets which will schlüpfen 
+// for that I need a pet class with 
+// ENUM and then the different pets. 
