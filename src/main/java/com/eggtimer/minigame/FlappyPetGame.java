7fd43134a281ab848d17f6d7c039026ed0def4a4
Antonia Spitzer <s2410238084@fhooe.at>
2026-08-31 20:01:07 +0200

    FlappyPetGame.java aktualisieren

--- src/main/java/com/eggtimer/minigame/FlappyPetGame.java
+++ src/main/java/com/eggtimer/minigame/FlappyPetGame.java
@@ -1,1 +1,26 @@
-// this will be one of the minigames
+// here will be the code for a flappy bird 
+// game
+
+
+// first we have to see what color your pet
+// has and which type of pet it is!
+// then we will use an image sequence. 
+// everytime you tap/you press the space bar
+// it will flap its arms down and back up
+
+// we have to get kind of a small physics engine 
+// bzw the pet/image should fall always 
+// the same (using a graph?)
+
+// there will be vertical 10 "blocks"
+// and there will be a random number
+// divided through 10 to get one of the
+// blocks not filled up, so the pet 
+// can get through it!
+
+// every zb 50 px there will be an
+// vertical block thing
+
+// for the game loop there will be some
+// kind of method so the "camera" will keep
+// continously move right
